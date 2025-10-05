d50ddcabd9e9a3edfb7ca30aa9421a6928baba37
Rishi Kumar B <bprtsp2022@gmail.com>
2025-10-05 22:50:50 +0530

    Add files via upload

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: " Which device is required for the internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1
+  },
+  {
+    question: "Which continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 4
+  },
+  {
+    question: "What does CPU stand for?",
+    option1: "Central Processing Unit",
+    option2: "Computer Personal Unit",
+    option3: "Central Peripheral Unit",
+    option4: "Control Processing Unit",
+    ans: 1
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    option1: "Venus",
+    option2: "Mars",
+    option3: "Jupiter",
+    option4: "Saturn",
+    ans: 2
+  },
+  {
+    question: "Which gas do plants absorb from the atmosphere?",
+    option1: "Oxygen",
+    option2: "Carbon Dioxide",
+    option3: "Nitrogen",
+    option4: "Hydrogen",
+    ans: 2
+  }
+];
